80029bbebe92bd4493a14fb5dec1979e095408df
FilipeCampos2424 <filipegames2343@gmail.com>
2026-03-17 10:56:39 -0300

    Exercícios 17 e 18 finalizados

---
+++ Ex_17.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex_17{
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Informe o valor da compra (R$): ");
+        double valor = leia.nextDouble();
+        double desconto = 0;
+        
+        if (valor > 500) {
+            desconto = valor * 0.20;
+            System.out.println("(Desconto de 20%)");
+        }
+        else if (valor >= 200) {
+            desconto = valor * 0.10 ;
+            System.out.println("(Desconto de 10%)");
+        }
+        else {
+            //sem desconto        
+        }
+        double valorFinal = valor - desconto;
+        System.out.println("Valor final da compra: "+valorFinal);  
+        leia.close();
+    }   
+}  
---
+++ Ex_18.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Ex_18{
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Informe um número inteiro de 1 a 10: ");
+        int numero = leia.nextInt();
+        if (numero < 1 || numero > 10)  {
+        System.out.println("Número inválido.");
+        }
+      
+        else {
+        switch (numero) {
+            case 1: 
+            System.out.println("1 = I");
+            break;
+
+            case 2: 
+            System.out.println("2 = II");
+            break;
+
+            case 3: 
+            System.out.println("3 = III");
+            break;
+
+            case 4: 
+            System.out.println("4 = IV");
+            break;
+
+            case 5: 
+            System.out.println("5 = V");
+            break;
+
+            case 6: 
+            System.out.println("6 = VI");
+            break;
+
+            case 7: 
+            System.out.println("7 = VII");
+            break; 
+
+            case 8: 
+            System.out.println("8 = VIII");
+            break; 
+
+            case 9: 
+            System.out.println("9 = IX");
+            break;
+
+            case 10: 
+            System.out.println("10 = X");
+            break;    
+            
+        }   
+          }
+        leia.close();
+        }
+
+    }   
