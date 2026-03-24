2a87d85080a6f3ad99cda70baf6fca080a0cd937
FilipeCampos2424 <filipegames2343@gmail.com>
2026-03-24 16:51:51 -0300

    Criação do arquivo, declarando váriaveis, criação da matriz da sala e criação do Menu

---
+++ TeatroMenu.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class TeatroMenu {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int NUM_SALAS = 2;
+
+        String[] nomesSalas = {
+                "Sala Beethoven",
+                "Sala Principal"
+        };
+
+        String[] espetaculos = {
+                "O Fantasma da Ópera",
+                "Hamlet"
+        };
+
+        double[] precoInteiro = {100, 120};
+
+        char[][][] cadeiras = new char[NUM_SALAS][12][12];
+
+        for (int s = 0; s < NUM_SALAS; s++) {
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    cadeiras[s][i][j] = ' ';
+                }
+            }
+        }
+
+        int opcao;
+
+        do {
+
+            System.out.println("\n===== TEATRO DEVISATE =====");
+            System.out.println("1 - Exibir mapa da sala");
+            System.out.println("2 - Reservar cadeira");
+            System.out.println("3 - Comprar cadeira");
+            System.out.println("4 - Cancelar reserva");
+            System.out.println("5 - Relatório financeiro");
+            System.out.println("0 - Sair");
+            System.out.println("\nEscolha: ");
+
+            opcao = leia.nextInt();
+
+    } while (opcao != 0);
+}
+}
